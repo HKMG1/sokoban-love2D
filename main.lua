075261d7fcae784f400c7d3d0cc22bf6c5b88c66
HKMG1 <166888374+HKMG1@users.noreply.github.com>
2025-08-13 03:30:55 +0800

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,49 +1,52 @@
-TileSet = love.graphics.newImage('/images/sokoban_tilesheet.png')
+-- const
 TileSize = 64
 
 function love.load()
+    tileSet = love.graphics.newImage('/images/sokoban_tilesheet.png')
+
     -- sprite pos in tilesheet
     quadInfo = {
-        {11, 6}, -- ground
-        {9, 6}, -- wall
-        {0, 3}, -- target
-        {1, 0}, -- box
-        {0, 4}, -- player
-        {1, 1}, -- box on target
+        { 11, 6 }, -- ground
+        { 9, 6 }, -- wall
+        { 0, 3 }, -- target
+        { 1, 0 }, -- crate
+        { 0, 4 }, -- player
+        { 1, 1 }, -- crate on target
     }
 
     -- objects ID
     -- 0 = ground
     -- 1 = wall
     -- 2 = target
-    -- 3 = box
+    -- 3 = crate
     -- 4 = player
-    -- 5 = box on target
+    -- 5 = crate on target
     -- 6 = player on target
     levels = {
-        -- level 1
+        -- level 1: 6x7
         {
-            {1, 1, 1, 1, 1, 1},
-            {1, 0, 2, 1, 1, 1},
-            {1, 0, 0, 1, 1, 1},
-            {1, 5, 0, 0, 4, 1},
-            {1, 0, 0, 3, 0, 1},
-            {1, 0, 0, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1}
+            { 1, 1, 1, 1, 1, 1 },
+            { 1, 0, 2, 1, 1, 1 },
+            { 1, 0, 0, 1, 1, 1 },
+            { 1, 5, 0, 0, 4, 1 },
+            { 1, 0, 0, 3, 0, 1 },
+            { 1, 0, 0, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1 }
         },
-        -- level 2
+        -- level 2: 6x7
         {
-            {1, 1, 1, 1, 1, 1},
-            {1, 0, 0, 0, 0, 1},
-            {1, 0, 1, 4, 0, 1},
-            {1, 0, 3, 5, 0, 1},
-            {1, 0, 2, 5, 0, 1},
-            {1, 0, 0, 0, 0, 1},
-            {1, 1, 1, 1, 1, 1}
+            { 1, 1, 1, 1, 1, 1 },
+            { 1, 0, 0, 0, 0, 1 },
+            { 1, 0, 1, 4, 0, 1 },
+            { 1, 0, 3, 5, 0, 1 },
+            { 1, 0, 2, 5, 0, 1 },
+            { 1, 0, 0, 0, 0, 1 },
+            { 1, 1, 1, 1, 1, 1 }
         }
     }
 
     levelIndex = 1
+    loadQuad()
     loadLevel()
 end
 
@@ -51,14 +54,15 @@ function love.draw()
     drawMap()
 end
 
-function loadLevel()
-    -- load images
+function loadQuad()
     quads = {}
     for i, info in ipairs(quadInfo) do
-        quads[i] = love.graphics.newQuad(info[1] * TileSize, info[2] * TileSize, TileSize, TileSize, TileSet:getDimensions())
+        quads[i] = love.graphics.newQuad(info[1] * TileSize, info[2] * TileSize, TileSize, TileSize, tileSet:getDimensions())
     end
-    
-    -- deep copy level
+end
+
+-- deep copy level
+function loadLevel()
     level = {}
     for y, row in ipairs(levels[levelIndex]) do
         level[y] = {}
@@ -66,6 +70,9 @@ function loadLevel()
             level[y][x] = cell
         end
     end
+
+    undoStack = {}
+    change = {}
 end
 
 function drawMap()
@@ -73,14 +80,12 @@ function drawMap()
         for rowIndex, cell in ipairs(row) do
             local x = (rowIndex - 1) * TileSize
             local y = (columnIndex - 1) * TileSize
-            love.graphics.draw(TileSet, quads[1], x, y)
-            if cell >= 1 and cell <= 4 then
-                love.graphics.draw(TileSet, quads[cell + 1], x, y)
-            elseif cell == 5 then
-                love.graphics.draw(TileSet, quads[6], x, y)
+            love.graphics.draw(tileSet, quads[1], x, y)
+            if cell >= 1 and cell <= 5 then
+                love.graphics.draw(tileSet, quads[cell + 1], x, y)
             elseif cell == 6 then
-                love.graphics.draw(TileSet, quads[3], x, y)
-                love.graphics.draw(TileSet, quads[5], x, y)
+                love.graphics.draw(tileSet, quads[3], x, y)
+                love.graphics.draw(tileSet, quads[5], x, y)
             end
         end
     end
@@ -88,17 +93,16 @@ end
 
 function love.keypressed(key)
     if key == 'up' or key == 'down' or key == 'left' or key == 'right' or key == 'w' or key == 'a' or key == 's' or key == 'd' then
-        local playerX = 0
-        local playerY = 0
+        local playerX, playerY
         local dx = 0
         local dy = 0
 
-        -- get current player position
-        for testY, row in ipairs(level) do
-            for testX, cell in ipairs(row) do
+        -- get player position
+        for y, row in ipairs(level) do
+            for x, cell in ipairs(row) do
                 if cell == 4 or cell == 6 then
-                    playerX = testX
-                    playerY = testY
+                    playerX = x
+                    playerY = y
                 end
             end
         end
@@ -116,52 +120,69 @@ function love.keypressed(key)
 
         -- get info about surroundings
         local current = level[playerY][playerX]
-        local adjacent = level[playerY + dy][playerX + dx]
-        local beyond = level[playerY + dy * 2][playerX + dx * 2]
+        local adjacent, beyond
+        if level[playerY + dy] and level[playerY + dy][playerX + dx] then
+            adjacent = level[playerY + dy][playerX + dx]
+        end
+        if level[playerY + dy * 2] and level[playerY + dy * 2][playerX + dx * 2] then
+            beyond = level[playerY + dy * 2][playerX + dx * 2]
+        end
 
         -- define moves
         local nextAdjacent = {
-            [0] = 4,
-            [2] = 6,
+            [0] = 4, -- [ > player | ] -> [ | player ]
+            [2] = 6, -- [ > player | target ] -> [ | player target ]
         }
         local nextCurrent = {
-            [4] = 0,
-            [6] = 2,
+            [4] = 0, -- [ > player | ] -> [ | player ]
+            [6] = 2, -- [ > player target | ] -> [ target | player ]
         }
         local nextBeyond = {
-            [0] = 3,
-            [2] = 5,
+            [0] = 3, -- [ > crate | ] -> [ | crate ]
+            [2] = 5, -- [ > crate | target ] -> [ | crate target ]
         }
-        local nextAdjacentinsert = {
-            [3] = 4,
-            [5] = 6,
+        local nextAdjacentInsert = {
+            [3] = 4, -- [ > crate | ] -> [ | crate ]
+            [5] = 6, -- [ > crate target ] -> [ target | crate ]
         }
+
+        -- save level
+        change[1] = {playerX, playerY, current}
+        change[2] = {playerX + dx, playerY + dy, adjacent}
+        change[3] = {playerX + dx * 2, playerY + dy * 2, beyond}
 
         -- implement moves
         if nextAdjacent[adjacent] then
             level[playerY][playerX] = nextCurrent[current]
             level[playerY + dy][playerX + dx] = nextAdjacent[adjacent]
-
-        elseif nextBeyond[beyond] and nextAdjacentinsert[adjacent] then
+            saveLevel()
+        elseif nextAdjacentInsert[adjacent] and nextBeyond[beyond] then
             level[playerY][playerX] = nextCurrent[current]
-            level[playerY + dy][playerX + dx] = nextAdjacentinsert[adjacent]
+            level[playerY + dy][playerX + dx] = nextAdjacentInsert[adjacent]
             level[playerY + dy * 2][playerX + dx * 2] = nextBeyond[beyond]
+            saveLevel()
         end
 
+        -- multipush
+        -- [ > movable | crate ] -> [ > movable | > crate ]
+        -- flood fill to find nearest non-crate
+
         -- check level win
-        local complete = true
+        -- all crate on target
+        local levelWin = true
 
         for y, row in ipairs(level) do
             for x, cell in ipairs(row) do
                 if cell == 2 or cell == 6 then
-                    complete = false -- all box on target
+                    levelWin = false
                 end
             end
         end
 
-        if complete then
+        if levelWin then
             levelIndex = levelIndex + 1
             if levelIndex > #levels then
+                -- should have ending
                 levelIndex = 1
             end
             loadLevel()
@@ -170,5 +191,23 @@ function love.keypressed(key)
     -- reset level
     elseif key == 'r' then
         loadLevel()
+    -- undo level
+    elseif key == 'z' then
+        undoLevel()
+    end
+end
+
+function saveLevel()
+    table.insert(undoStack, change)
+    change = {}
+end
+
+function undoLevel()
+    if #undoStack > 0 then
+        while #undoStack[#undoStack] > 0 do
+            local change = table.remove(undoStack[#undoStack])
+            level[change[2]][change[1]] = change[3]
+        end
+        table.remove(undoStack)
     end
 end
